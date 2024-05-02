6b1d1d26b88c50bb1e3ae49e471e84601813e99f
Ryan <ryan.irani@gmail.com>
2024-05-02 15:07:46 +0100

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -3,15 +3,8 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home'
-import EtchASketch from './pages/Etch-a-sketch'
-import Library from './pages/Book-library'
-import Header from './components/Header'
 import About from './components/About'
-import Navigation from './components/Navigation';
-import Skills from './components/Skills'
-import Project from './components/Project'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+
 
 
 
