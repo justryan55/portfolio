b5f62545ca2988d3e3184532b77ff12989bb74d5
Ryan <ryan.irani@gmail.com>
2024-05-22 22:51:31 +0100

    mobile friendly

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
     return (
         <header className="header">
             <span>Ryan Irani</span>
-            <span>Full Stack Web Developer</span>
+            <span className='second-header'>Full Stack Web Developer</span>
             <p className="intro-text">From mending bodies to crafting pixels, I'm a frontend developer with a unique journey. As a former physiotherapist turned coding enthusiast, I bring a blend of precision, creativity, and empathy to every project.</p>
             <BouncingArrow />
         </header>
