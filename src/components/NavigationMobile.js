12322950f9d6025b0edb667d88551941c52ba8a7
Ryan <ryan.irani@gmail.com>
2024-05-23 18:00:21 +0100

    linked up mob nav

--- src/components/NavigationMobile.js
+++ src/components/NavigationMobile.js
@@ -8,6 +8,8 @@ import home from '../images/home.svg';
 import about from '../images/user.svg';
 import projects from '../images/hard-drive.svg';
 import contact from '../images/mail.svg';
+import { Link as ScrollLink } from "react-scroll";
+
 
 export const NavigationMobile = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -27,10 +29,10 @@ export const NavigationMobile = () => {
                         transition={{ duration: 0.2 }}
                     >
                      <ul className="mobile-menu">
-                        <li><img src={home} alt='home'/>Home</li>
-                        <li><img src={about} alt='about'/>About</li>
-                        <li><img src={projects} alt='projects'/>Projects</li>
-                        <li><img src={contact} alt='contact'/>Contact</li>
+                        <li><ScrollLink to='header' smooth={true} duration={500}><img src={home} alt='home'/>Home</ScrollLink></li>
+                        <li><ScrollLink to='about' smooth={true} duration={500}><img src={about} alt='about'/>About</ScrollLink></li>
+                        <li><ScrollLink to='projects' smooth={true} duration={500}><img src={projects} alt='projects'/>Projects</ScrollLink></li>
+                        <li><ScrollLink to='contact' smooth={true} duration={500}><img src={contact} alt='contact'/>Contact</ScrollLink></li>
                     </ul>
 
                         <motion.li
@@ -50,3 +52,4 @@ export const NavigationMobile = () => {
         </div>
     );
 };
+
